fd6cf3378ae9fe514c0db19a303073934ba13766
Thomas Schwab <thomasschwab146@gmail.com>
2025-05-03 21:11:39 +0200

    Adding two new queries and their results

---
+++ queries/all_clients_multiple_orders.sql
@@ -0,0 +1,7 @@
+USE kamilah_schwab_books;
+SELECT c.name AS customer,
+    SUM(o.quantity) AS total_ordered
+FROM clients AS c
+    JOIN orders AS o ON c.id = o.client_id
+GROUP BY c.name
+HAVING SUM(o.quantity) > 1;
---
+++ queries/sum_order_total.sql
@@ -0,0 +1,3 @@
+USE kamilah_schwab_books;
+SELECT SUM(total) AS total_revenue
+FROM orders;
